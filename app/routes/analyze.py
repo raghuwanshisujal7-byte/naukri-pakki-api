ab7bf7df7c87024c7588f04ce40fb2da44875960
raghuwanshisujal7-byte <raghuwanshisujal7@gmail.com>
2025-12-27 23:54:34 +0530

    Add analyze route

---
+++ app/routes/analyze.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/analyze",
+    tags=["Analyze"]
+)
+
+@router.get("/")
+def analyze_test():
+    return {"message": "Analyze route is working"}
